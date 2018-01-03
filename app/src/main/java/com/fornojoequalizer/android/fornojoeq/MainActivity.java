7591fe0e610c9c1824c39be274bcab7adf520c9e
danisharsalan <danisharsalan@yahoo.com>
2018-01-02 22:58:24 -0500

    simple android equalizer

---
+++ app/src/main/java/com/fornojoequalizer/android/fornojoeq/MainActivity.java
@@ -0,0 +1,289 @@
+package com.fornojoequalizer.android.fornojoeq;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.audiofx.Equalizer;
+import android.media.audiofx.Visualizer;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final float VISUALIZER_HEIGHT_DIP = 50f;
+
+    private MediaPlayer mMediaPlayer;
+    private Visualizer mVisualizer;
+    private Equalizer mEqualizer;
+
+    private LinearLayout mLinearLayout;
+    private VisualizerView mVisualizerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        requestAudioPermissions();
+
+    }
+
+
+
+    private void setupVisualizerFxAndUI() {
+        mLinearLayout = (LinearLayout) findViewById(R.id.linearLayoutVisual);
+        mVisualizerView = new VisualizerView(this);
+        mVisualizerView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                (int) (VISUALIZER_HEIGHT_DIP * getResources().getDisplayMetrics().density)));
+        mLinearLayout.addView(mVisualizerView);
+
+        mVisualizer = new Visualizer(mMediaPlayer.getAudioSessionId());
+        mVisualizer.setCaptureSize(Visualizer.getCaptureSizeRange()[1]);
+
+        mVisualizer.setDataCaptureListener(new Visualizer.OnDataCaptureListener() {
+            public void onWaveFormDataCapture(Visualizer visualizer, byte[] bytes,
+                                              int samplingRate) {
+                mVisualizerView.updateVisualizer(bytes);
+            }
+
+            public void onFftDataCapture(Visualizer visualizer, byte[] bytes, int samplingRate) {
+            }
+        }, Visualizer.getMaxCaptureRate() / 2, true, false);
+    }
+
+    private void setupEqualizerFxAndUI() {
+
+        mLinearLayout = (LinearLayout) findViewById(R.id.linearLayoutEqual);
+
+        TextView equalizerHeading = new TextView(this);
+        equalizerHeading.setText("Equalizer");
+        equalizerHeading.setTextSize(20);
+        equalizerHeading.setGravity(Gravity.CENTER_HORIZONTAL);
+        mLinearLayout.addView(equalizerHeading);
+
+        short numberFrequencyBands = mEqualizer.getNumberOfBands();
+
+        final short lowerEqualizerBandLevel = mEqualizer.getBandLevelRange()[0];
+        final short upperEqualizerBandLevel = mEqualizer.getBandLevelRange()[1];
+
+        for (short i = 0; i < numberFrequencyBands; i++) {
+            final short equalizerBandIndex = i;
+
+            TextView frequencyHeaderTextview = new TextView(this);
+            frequencyHeaderTextview.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            frequencyHeaderTextview.setGravity(Gravity.CENTER_HORIZONTAL);
+            frequencyHeaderTextview
+                    .setText((mEqualizer.getCenterFreq(equalizerBandIndex) / 1000) + " Hz");
+            mLinearLayout.addView(frequencyHeaderTextview);
+
+            LinearLayout seekBarRowLayout = new LinearLayout(this);
+            seekBarRowLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+            TextView lowerEqualizerBandLevelTextview = new TextView(this);
+            lowerEqualizerBandLevelTextview.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            lowerEqualizerBandLevelTextview.setText((lowerEqualizerBandLevel / 100) + " dB");
+            TextView upperEqualizerBandLevelTextview = new TextView(this);
+            upperEqualizerBandLevelTextview.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            upperEqualizerBandLevelTextview.setText((upperEqualizerBandLevel / 100) + " dB");
+
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.weight = 1;
+
+            SeekBar seekBar = new SeekBar(this);
+            seekBar.setId(i);
+
+            seekBar.setLayoutParams(layoutParams);
+            seekBar.setMax(upperEqualizerBandLevel - lowerEqualizerBandLevel);
+            seekBar.setProgress(mEqualizer.getBandLevel(equalizerBandIndex));
+
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                public void onProgressChanged(SeekBar seekBar, int progress,
+                                              boolean fromUser) {
+                    mEqualizer.setBandLevel(equalizerBandIndex,
+                            (short) (progress + lowerEqualizerBandLevel));
+                }
+
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                    //not used
+                }
+
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    //not used
+                }
+            });
+
+            seekBarRowLayout.addView(lowerEqualizerBandLevelTextview);
+            seekBarRowLayout.addView(seekBar);
+            seekBarRowLayout.addView(upperEqualizerBandLevelTextview);
+
+            mLinearLayout.addView(seekBarRowLayout);
+
+            equalizeSound();
+        }
+    }
+
+    private void equalizeSound() {
+        ArrayList<String> equalizerPresetNames = new ArrayList<String>();
+        ArrayAdapter<String> equalizerPresetSpinnerAdapter
+                = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,
+                equalizerPresetNames);
+        equalizerPresetSpinnerAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner equalizerPresetSpinner = (Spinner) findViewById(R.id.spinner);
+
+        for (short i = 0; i < mEqualizer.getNumberOfPresets(); i++) {
+            equalizerPresetNames.add(mEqualizer.getPresetName(i));
+        }
+
+        equalizerPresetSpinner.setAdapter(equalizerPresetSpinnerAdapter);
+
+        equalizerPresetSpinner.setOnItemSelectedListener(new AdapterView
+                .OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent,
+                                       View view, int position, long id) {
+                mEqualizer.usePreset((short) position);
+                short numberFrequencyBands = mEqualizer.getNumberOfBands();
+                final short lowerEqualizerBandLevel = mEqualizer.getBandLevelRange()[0];
+
+                for (short i = 0; i < numberFrequencyBands; i++) {
+                    short equalizerBandIndex = i;
+                    SeekBar seekBar = (SeekBar) findViewById(equalizerBandIndex);
+                    seekBar.setProgress(mEqualizer
+                            .getBandLevel(equalizerBandIndex) - lowerEqualizerBandLevel);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //not used
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (isFinishing() && mMediaPlayer != null) {
+            mVisualizer.release();
+            mEqualizer.release();
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
+
+    private final int MY_PERMISSIONS_RECORD_AUDIO = 1;
+
+    private void requestAudioPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            //When permission is not granted by user, show them message why this permission is needed.
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)) {
+                Toast.makeText(this, "Please grant permissions to record audio", Toast.LENGTH_LONG).show();
+
+                //Give user option to still opt-in the permissions
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_RECORD_AUDIO);
+
+            } else {
+                // Show user dialog to grant permission to record audio
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_RECORD_AUDIO);
+            }
+        }
+        //If permission is granted, then go ahead recording audio
+        else if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            //Go ahead with recording audio now
+            setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+            mMediaPlayer = MediaPlayer.create(this, R.raw.test_audio_file);
+
+            mMediaPlayer.start();
+
+            mEqualizer = new Equalizer(0, mMediaPlayer.getAudioSessionId());
+            mEqualizer.setEnabled(true);
+
+            setupVisualizerFxAndUI();
+            setupEqualizerFxAndUI();
+
+            mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    mVisualizer.setEnabled(false);
+                }
+            });
+        }
+    }
+
+    //Handling callback
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_RECORD_AUDIO: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay!
+                    setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+                    mMediaPlayer = MediaPlayer.create(this, R.raw.test_audio_file);
+
+                    mMediaPlayer.start();
+
+                    mEqualizer = new Equalizer(0, mMediaPlayer.getAudioSessionId());
+                    mEqualizer.setEnabled(true);
+
+                    setupVisualizerFxAndUI();
+                    setupEqualizerFxAndUI();
+
+                    mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        public void onCompletion(MediaPlayer mediaPlayer) {
+                            mVisualizer.setEnabled(false);
+                        }
+                    });
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "Permissions Denied to record audio", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+}
